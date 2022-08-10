715c64f0ee9ca49287f74281c9020061ec8c267f
Rafal Matyjasz <rmatyjas@cisco.com>
2022-08-10 10:53:39 +0200

    Troubleshoot Terraform

--- main.tf
+++ main.tf
@@ -12,6 +12,13 @@ provider "aws" {
   region = var.region
 }
 
+locals {
+  security_groups = {
+    sg_ping = aws_security_group.sg_ping.id,
+    sg_8080 = aws_security_group.sg_8080.id,
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -33,10 +40,12 @@ data "http" "myip" {
 }
 
 resource "aws_instance" "web_app" {
-  for_each               = aws_security_group.*.id
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [each.id]
+  # for_each               = aws_security_group.*.id
+  for_each      = local.security_groups
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  # vpc_security_group_ids = [each.id]
+  vpc_security_group_ids = [each.value]
   user_data              = <<-EOF
               #!/bin/bash
               apt-get update
@@ -45,31 +54,32 @@ resource "aws_instance" "web_app" {
               echo "Hello World" > /var/www/html/index.html
               systemctl restart apache2
               EOF
- tags = {
-    Name = $var.name-learn
+  tags = {
+    # Name = "${var.name}-learn"
+    Name = "${var.name}-learn-${each.key}"
   }
 }
 
 resource "aws_security_group" "sg_ping" {
   name = "Allow Ping"
 
-  ingress {
-    from_port       = -1
-    to_port         = -1
-    protocol        = "icmp"
-    security_groups = [aws_security_group.sg_8080.id]
-  }
+  # ingress {
+  #   from_port       = -1
+  #   to_port         = -1
+  #   protocol        = "icmp"
+  #   security_groups = [aws_security_group.sg_8080.id]
+  # }
 }
 
 resource "aws_security_group" "sg_8080" {
   name = "Allow 8080"
 
-  ingress {
-    from_port       = 8080
-    to_port         = 8080
-    protocol        = "tcp"
-    security_groups = [aws_security_group.sg_ping.id]
-  }
+  # ingress {
+  #   from_port       = 8080
+  #   to_port         = 8080
+  #   protocol        = "tcp"
+  #   security_groups = [aws_security_group.sg_ping.id]
+  # }
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
   egress {
     from_port   = 0
@@ -80,20 +90,37 @@ resource "aws_security_group" "sg_8080" {
 }
 
 resource "aws_security_group_rule" "allow_localhost_8080" {
-  type = "ingress"
-  from_port = 8080
-  to_port = 8080                            
-  protocol = "tcp"
-  cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["${chomp(data.http.myip.body)}/32"]
   security_group_id = aws_security_group.sg_8080.id
 }
 
 resource "aws_security_group_rule" "allow_localhost_ping" {
-  type = "ingress"
-  from_port = -1
-  to_port = -1
-  protocol = "icmp"
-  cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+  type              = "ingress"
+  from_port         = -1
+  to_port           = -1
+  protocol          = "icmp"
+  cidr_blocks       = ["${chomp(data.http.myip.body)}/32"]
   security_group_id = aws_security_group.sg_ping.id
 }
 
+resource "aws_security_group_rule" "allow_ping" {
+  type                     = "ingress"
+  from_port                = -1
+  to_port                  = -1
+  protocol                 = "icmp"
+  security_group_id        = aws_security_group.sg_ping.id
+  source_security_group_id = aws_security_group.sg_8080.id
+}
+
+resource "aws_security_group_rule" "allow_8080" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.sg_8080.id
+  source_security_group_id = aws_security_group.sg_ping.id
+}
